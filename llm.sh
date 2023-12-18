d2bcb3366c79bddfed301d9f5a2eeaa5e00e9606
jfeuillatre <jfeuillatre@jfeuillatre.home>
2023-12-18 12:30:18 +0100

    [SH] changed model

--- llm.sh
+++ llm.sh
@@ -2,13 +2,13 @@
 # https://huggingface.co/TheBloke/OpenHermes-2.5-Mistral-7B-GGUF/tree/main
 # 8GB RAM : Q6 quant for GPU and Q4_K_M for CPU
 
-# MODEL=openhermes-2.5-mistral-7b.Q3_K_M.gguf
-# wget https://huggingface.co/TheBloke/OpenHermes-2.5-Mistral-7B-GGUF/resolve/main/${MODEL}
+MODEL=openhermes-2.5-mistral-7b.Q3_K_M.gguf
+wget https://huggingface.co/TheBloke/OpenHermes-2.5-Mistral-7B-GGUF/resolve/main/${MODEL}
 
 # https://gpt4all.io
 # 1GB RAM : https://gpt4all.io/models/gguf/all-MiniLM-L6-v2-f16.gguf
-MODEL=all-MiniLM-L6-v2-f16.gguf
-wget https://gpt4all.io/models/gguf/${MODEL}
+# MODEL=all-MiniLM-L6-v2-f16.gguf
+# wget https://gpt4all.io/models/gguf/${MODEL}
 
 # compile llama.cpp
 # https://github.com/ggerganov/llama.cpp
